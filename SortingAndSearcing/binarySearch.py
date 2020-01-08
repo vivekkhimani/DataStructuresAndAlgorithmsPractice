d43984a5c26da5f4618199d6824b2665eb85bf30
Khimani <vck29@drexel.edu>
2020-01-08 18:02:42 -0500

    updated the binary search algorithm

--- SortingAndSearcing/binarySearch.py
+++ SortingAndSearcing/binarySearch.py
@@ -1,27 +1,33 @@
 #BinarySearch (Algorithm Description) - Look for the element at the centre, if not found, further divide the array in two parts, and keep on repeating the process until the element is found. THIS ONLY WORKS ON SORTED ARRAYS and CAN BE DONE USING RECURSION OR LOOPS
-myList = [10,9,4,1,89,45,67,23,12,76]
-myList.sort()
 
-def binarySearch(inputList,key):
-  firstIndex = 0
-  lastIndex = len(inputList) - 1
-  middle = (firstIndex + lastIndex)//2        #floor division as indexes can only have int values
-  found = "NOT FOUND"
+import math
 
-  if len(inputList) == 0:
-    return found
+def binarySearch(input_list,key):
+    
 
-	if key == inputList[middle]:
-		found = 'FOUND'
-		return found
-  
-	elif key < inputList[middle]:
-		binarySearch(inputList[0:middle],key)
-  
-	elif key > inputList[middle]:
-		binarySearch(inputList[(middle+1):],key)
 
-  
+    while len(input_list) >= 1:
+        start = 0
+        end = len(input_list) - 1
+        mid = (start + end)/2
+        mid = math.floor(mid)
 
-print(len(
-print(binarySearch(myList,76))
+        if (input_list[mid] == key):
+            return "FOUND"
+
+        elif (key < input_list[mid]):
+            input_list = input_list[0:mid]
+
+        elif (key > input_list[mid]):
+            input_list = input_list[(mid+1):(end+1)]
+
+                
+    return "NOT FOUND"
+
+
+
+##DRIVER##
+my_list = [10,9,4,1,89,45,67,23,12,76]
+my_list.sort()
+
+print(binarySearch(my_list,90))
