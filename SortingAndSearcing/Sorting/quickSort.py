c62181ac63e1c26fbb7bdea916795946bfaa93e5
Vivek Khimani <vivekkhimani07@gmail.com>
2020-01-20 19:34:57 -0500

    added quick sort

--- SortingAndSearcing/Sorting/quickSort.py
+++ SortingAndSearcing/Sorting/quickSort.py
@@ -1,21 +1,22 @@
 '''
 Description:
-quickSort (Algorithm Description) - 
+quickSort (Algorithm Description) -  It picks an element as pivot and partitions the given array around the picked pivot.
 
-Average Time Complexity: 
+Average Time Complexity: 	O(nlogn)
+Worst Case Time Complexity: O(n^2)
 
-Tip: 
+Tip: One of the fastest comparison-based sorting algorithm but should try and avoid using it because of its high worst-case time complexity
 '''
 
 def quickSort(inputList,start,stop):
 	if start<stop:
-		p = partition(inputList,start,stop-1)
+		p = partition(inputList,start,stop)
 		quickSort(inputList,start,p-1)
-		quickSort(inputList,p+1,stop-1)
+		quickSort(inputList,p+1,stop)
 
 def partition(inputList,start,stop):
 	#pivot = last element
-	pivot = inputList[stop-1]
+	pivot = inputList[stop]
 	i = start
 	for j in range(start,stop):
 		if inputList[j] <= pivot:
@@ -25,8 +26,8 @@ def partition(inputList,start,stop):
 			i+=1
 
 	temp=inputList[i]
-	inputList[i]=inputList[stop-1]
-	inputList[stop-1]=temp
+	inputList[i]=inputList[stop]
+	inputList[stop]=temp
 	return i
 
 
@@ -34,6 +35,6 @@ def partition(inputList,start,stop):
 if __name__ == '__main__':
 	my_list = [10,9,4,1,89,45,67,23,12,76]
 	start = 0
-	stop = len(my_list)
+	stop = len(my_list) - 1
 	quickSort(my_list,start,stop)
 	print(my_list)
